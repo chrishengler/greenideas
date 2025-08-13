8b8aed1a89bb96be92ac85f3455f914ab6d19472
Chris Hengler <chrysics@gmail.com>
2025-08-13 13:05:39 +0200

    case in prepositional phrase expansion

--- greenideas/rules/default_english_rules/pp_expansions.py
+++ greenideas/rules/default_english_rules/pp_expansions.py
@@ -1,3 +1,5 @@
+from greenideas.attributes.attribute_type import AttributeType
+from greenideas.attributes.case import Case
 from greenideas.parts_of_speech.pos_types import POSType
 from greenideas.rules.expansion_spec import ExpansionSpec
 from greenideas.rules.grammar_rule import GrammarRule
@@ -5,7 +7,11 @@ from greenideas.rules.source_spec import SourceSpec
 
 # PP -> Prep NP
 pp__prep_np = GrammarRule(
-    SourceSpec(POSType.PP), [ExpansionSpec(POSType.Prep), ExpansionSpec(POSType.NP)]
+    SourceSpec(POSType.PP),
+    [
+        ExpansionSpec(POSType.Prep),
+        ExpansionSpec(POSType.NP, {AttributeType.CASE: Case.OBJECTIVE}),
+    ],
 )
 
 pp_expansions = [pp__prep_np]
