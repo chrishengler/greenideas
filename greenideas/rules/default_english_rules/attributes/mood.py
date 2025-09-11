94613fe5b345ec2b650272ff82476e85368c2439
Chris Hengler <chrysics@gmail.com>
2025-09-11 12:24:56 +0200

    s conj q

--- greenideas/rules/default_english_rules/attributes/mood.py
+++ greenideas/rules/default_english_rules/attributes/mood.py
@@ -3,4 +3,4 @@ from greenideas.attributes.grammatical_attribute import GrammaticalAttribute
 
 class Mood(GrammaticalAttribute):
     DECLARATIVE = ("declarative", 0.8)
-    INTERROGATIVE = ("interrogative", 100.2)
+    INTERROGATIVE = ("interrogative", 0.2)
--- greenideas/rules/default_english_rules/expansions/utterance_expansions.py
+++ greenideas/rules/default_english_rules/expansions/utterance_expansions.py
@@ -27,7 +27,19 @@ u__q = GrammarRule(
     [ExpansionSpec(DefaultEnglishPOSType.Q, post_punctuation="?")],
 )
 
-u_expansions = [
-    u__s,
-    u__q,
-]
+u__s_conj_q = GrammarRule(
+    SourceSpec(
+        DefaultEnglishPOSType.Utterance,
+        {
+            DefaultEnglishAttributeType.MOOD: Mood.INTERROGATIVE,
+        },
+    ),
+    [
+        ExpansionSpec(DefaultEnglishPOSType.S, post_punctuation=","),
+        ExpansionSpec(DefaultEnglishPOSType.SimpleConj),
+        ExpansionSpec(DefaultEnglishPOSType.Q, post_punctuation="?"),
+    ],
+    weight=0.2,
+)
+
+u_expansions = [u__s, u__q, u__s_conj_q]
